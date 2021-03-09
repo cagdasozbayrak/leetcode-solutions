991155319afc9a6a3b65f5dc0fcc46ae94145c1d
cagdasozbayrak <cagdas.ozbayrak@outlook.com>
2021-03-09 11:47:18 +0300

    palindrome number solution with converting to string

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,23 @@
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        String s = String.valueOf(x);
+
+        int length = s.length();
+        int l = 0;
+        int r = length - 1;
+
+        while (l <= r && r > 0 && r < length) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
